ce099776f7d3a4afa55ff49c7f1104b31b5631c0
franchialan <franchialan@outlook.com>
2023-10-16 17:38:16 -0300

    change orders

--- src/controllers/orders_controller.js
+++ src/controllers/orders_controller.js
@@ -1,7 +1,7 @@
 const get = (req, res, next) => {
   try {
     const orders = [
-      { id: '1', productId: '1a', orderFor: 'Bruce Wayne', deliveryAddress: 'Wayne Manor', deliveryDate: '02/11/2022', deliveryType: 'STANDARD' },
+      { id: '1', productId: '1a', orderFor: 'Batman', deliveryAddress: 'Wayne Manor', deliveryDate: '02/11/2022', deliveryType: 'STANDARD' },
       { id: '2', productId: '1b', orderFor: 'Steve Rogers', deliveryAddress: 'Avengers HQ', deliveryDate: '10/10/2022', deliveryType: 'FAST DELIVERY' },
       { id: '3', productId: '1c', orderFor: 'Tony Stark', deliveryAddress: 'Stark Tower', deliveryDate: '02/10/2022', deliveryType: 'STANDARD' },
       { id: '4', productId: '1d', orderFor: 'Natasha Romanoff', deliveryAddress: 'Avengers HQ', deliveryDate: '22/09/2022', deliveryType: 'FAST DELIVERY' },
